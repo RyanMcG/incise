56be9f44beef1d83dfd5d4e7fa184795d13cba36
Ryan McGowan <ryan@ryanmcg.com>
2013-08-11 14:42:33 -0700

    Fill out core_spec for finding parsers and layouts.

--- spec/incise/core_spec.clj
+++ spec/incise/core_spec.clj
@@ -5,10 +5,25 @@
 (describe "namespace is layout or parser filter"
   (it "returns false on non-parser and non-layout namespaces"
     (doseq [ns-sym ['arbitrary.name.space 'my.parser.yay 'your.layout.boop]]
-      (should-not (namespace-is-layout-or-parser ns-sym))))
+      (should-not (namespace-is-layout-or-parser? ns-sym))))
   (it "returns true for parser and layout namespaces"
     (doseq [ns-sym ['incise.parsers.cool-thing 'incise.layouts.bootstrapped]]
-      (should (namespace-is-layout-or-parser ns-sym))))
-  (it "returns false for core parser and layout namespaces"
-    (doseq [core-ns core-namespace-symbols]
-      (should-not (namespace-is-layout-or-parser core-ns)))))
+      (should (namespace-is-layout-or-parser? ns-sym)))))
+
+(describe "namespace is spec or test"
+  (it "returns true for specs and tests"
+    (doseq [ns-sym ['cool.thing-test 'incise.parsers.core-spec]]
+      (should (namespace-is-spec-or-test? ns-sym))))
+  (it "returns false for other namespaces"
+    (doseq [ns-sym ['cool.thing 'incise.parsers.batman]]
+      (should-not (namespace-is-spec-or-test? ns-sym)))))
+
+(describe "finding parsers and layouts"
+  (with layout-and-parser-syms (find-parser-and-layout-symbols))
+  (it "does not contain core namespaces"
+    (doseq [core-sym core-namespace-symbols]
+      (should-not-contain core-sym @layout-and-parser-syms)))
+  (it "should contain default layouts and parsers"
+    (doseq [default-sym ['incise.parsers.markdown
+                         'incise.layouts.page]]
+      (should-contain default-sym @layout-and-parser-syms))))
