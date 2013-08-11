abe43504c4a65fe8def510efdbec928438201351
Ryan McGowan <ryan@ryanmcg.com>
2013-08-11 14:42:09 -0700

    Avoid requiring spec and test namespaces.

--- src/incise/core.clj
+++ src/incise/core.clj
@@ -16,13 +16,25 @@
   [namespace-sym]
   (re-find #"incise\.(layouts|parsers)\..+" (str namespace-sym)))
 
+(defn namespace-is-spec-or-test?
+  "Predicate to determine if the given namespace is a spec or test."
+  [namespace-sym]
+  (re-find #"-(test|spec)$" (str namespace-sym)))
+
+(defn find-parser-and-layout-symbols
+  "Find symbols for namespaces on the classpath that are valid layouts and
+   parsers."
+  []
+  (->> (classpath)
+       (ns-tools/find-namespaces)
+       (remove (partial contains? core-namespace-symbols))
+       (remove namespace-is-spec-or-test?)
+       (filter namespace-is-layout-or-parser?)))
+
 (defn load-parsers-and-layouts
   "Require with reload all namespaces under incise.layouts and incise.parsers."
   []
-  (doseq [ns-sym (->> (classpath)
-                      (ns-tools/find-namespaces)
-                      (remove (partial contains? core-namespace-symbols))
-                      (filter namespace-is-layout-or-parser?)) ]
+  (doseq [ns-sym (find-parser-and-layout-symbols)]
     (require :reload ns-sym)))
 
 (load-parsers-and-layouts)
