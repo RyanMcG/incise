f65c68d34147c910255c4e692fbe8af72cb5c7cc
Ryan McGowan <ryan@ryanmcg.com>
2013-08-12 01:52:18 -0700

    Add blog post to spec resources for testing.

--- spec/incise/parsers/markdown_spec.clj
+++ spec/incise/parsers/markdown_spec.clj
@@ -1,10 +1,12 @@
 (ns incise.parsers.markdown-spec
   (:require [speclj.core :refer :all]
+            [clojure.java.io :refer [file resource]]
             [incise.parsers.markdown :refer :all])
   (:import [java.io File]))
 
 (describe "parsing"
+  (with markdown-file (file (resource "spec/another-forgotten-binding-pry.md")))
   (it "does something"
-    (should (parse (File. "hey")))))
+    (should (parse @markdown-file))))
 
 (run-specs)
