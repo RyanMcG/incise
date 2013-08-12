d728505c22e05ebb921a244c42423be7ad692460
Ryan McGowan <ryan@ryanmcg.com>
2013-08-12 02:05:31 -0700

    Make layouts use strings by calling name.

--- src/incise/layouts/core.clj
+++ src/incise/layouts/core.clj
@@ -6,13 +6,13 @@
 (defn exists?
   "Check for the existance of a layout with the given name."
   [layout-with-name]
-  (contains? @layouts layout-with-name))
+  (contains? @layouts (name layout-with-name)))
 
-(defn get [& args]
-  (apply clojure.core/get @layouts args))
+(defn get [layout-name]
+  (@layouts (name layout-name)))
 
 (defn register
   "Register a layout function to a shortname"
   [short-name layout-fn]
   (swap! layouts
-         assoc (str short-name) layout-fn))
+         assoc (name short-name) layout-fn))
