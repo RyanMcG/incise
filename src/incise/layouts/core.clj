ec59b732196d3eaecb1ec4cb9b55f7a39f95b118
Ryan McGowan <ryan@ryanmcg.com>
2013-08-12 01:36:29 -0700

    Add exists? and get functions to layouts core.

--- src/incise/layouts/core.clj
+++ src/incise/layouts/core.clj
@@ -1,6 +1,15 @@
-(ns incise.layouts.core)
+(ns incise.layouts.core
+  (:refer-clojure :exclude [get]))
 
 (def layouts (atom {}))
+
+(defn exists?
+  "Check for the existance of a layout with the given name."
+  [layout-with-name]
+  (contains? @layouts layout-with-name))
+
+(defn get [& args]
+  (apply clojure.core/get @layouts args))
 
 (defn register
   "Register a layout function to a shortname"
