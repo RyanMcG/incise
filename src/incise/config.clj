dccfe69346446b9118174fdf1f9b93245df3fd7d
Ryan McGowan <ryan@ryanmcg.com>
2013-08-17 02:30:00 -0700

    Add incise.config.

--- src/incise/config.clj
+++ src/incise/config.clj
@@ -1,3 +1,12 @@
-(ns incise.config)
+(ns incise.config
+  (:refer-clojure :exclude [assoc get]))
 
 (def config (atom {}))
+
+(defn assoc [& args]
+  (apply swap! config assoc args))
+
+(defn get [& args]
+  (if (empty? args)
+    @config
+    (apply @config args)))
