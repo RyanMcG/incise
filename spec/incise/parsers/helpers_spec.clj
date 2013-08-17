ad0a5854454ddeeec10e2ed5b3a6b49c5d85e136
Ryan McGowan <ryan@ryanmcg.com>
2013-08-17 02:36:22 -0700

    Add parsers.helpers and specs for them.

---
+++ spec/incise/parsers/helpers_spec.clj
@@ -0,0 +1,42 @@
+(ns incise.parsers.helpers-spec
+  (:require [speclj.core :refer :all]
+            [clj-time.core :refer [date-time]]
+            [clojure.java.io :refer [file]]
+            (incise.parsers [core :refer [map->Parse]]
+                            [helpers :refer :all])))
+
+(describe "str->date-time"
+  (it "makes a DateTime from a string"
+    (should= (date-time 2013 8 12) (str->date-time "2013-08-12"))))
+
+(describe "date-str->path"
+  (it "handles empty dates well"
+    (should= "" (date-str->path "")))
+  (it "basically works"
+    (should= "/2013/8/12" (date-str->path "2013-08-12"))))
+
+(describe "Parse->path"
+  (it "works with dated parses"
+    (should= "/2013/8/12/something-about-binding-pry/index.html"
+             (Parse->path (map->Parse {:title "Something about binding.pry"
+                                       :date "2013-8-12"
+                                       :extension "/index.html"}))))
+  (it "works with undated parses"
+    (should= "/run-a-muck/index.html"
+             (Parse->path (map->Parse {:title "Run a Muck"
+                                       :date ""
+                                       :extension "/index.html"})))
+    (should= "/nilnilnil/index.html"
+             (Parse->path (map->Parse {:title "NilNilNil"
+                                       :date nil
+                                       :extension "/index.html"})))))
+(describe "extension"
+  (with coffee-file (file "boom/my/coolio.js.coffee"))
+  (it "gets the extension of a file"
+    (should= "coffee" (extension @coffee-file)))
+
+  (with md-file (file "my.articles/wow.pants.MARKDOWN"))
+  (it "enforces a lower-case extension"
+    (should= "markdown" (extension @md-file))))
+
+(run-specs)
---
+++ src/incise/parsers/helpers.clj
@@ -0,0 +1,34 @@
+(ns incise.parsers.helpers
+  (:require (clj-time [core :as tm]
+                      [coerce :as tc])
+            [clojure.string :as s])
+  (:import [java.io File]))
+
+(defn dashify
+  "Dashify a title, replacing all non word characters with a dash."
+  [^String title]
+  (str \/ (-> title
+              (s/lower-case)
+              (s/replace #"[^\w]" "-"))))
+
+(defn date-time->path
+  "Convert a DateTime to a path like string."
+  [date-time]
+  (str (when date-time
+         (str \/ (tm/year date-time)
+              \/ (tm/month date-time)
+              \/ (tm/day date-time)))))
+
+(def date-str->path (comp date-time->path tc/from-string))
+
+(defn Parse->path
+  [{:keys [date title extension]}]
+  (str (date-str->path date) (dashify title) extension))
+
+(defn extension [^File file]
+  "Get the extension, enforcing lower case, on the given file."
+  (-> file
+      (.getName)
+      (s/split #"\.")
+      (last)
+      (s/lower-case)))
