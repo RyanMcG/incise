6308f007112ff906d7ae8b1d4822bb58bfa69816
Ryan McGowan <ryan@ryanmcg.com>
2013-08-17 02:35:52 -0700

    Add log-exceptions wrapper to watch.

--- src/incise/watcher.clj
+++ src/incise/watcher.clj
@@ -2,11 +2,19 @@
   (:require [watchtower.core :refer [watcher rate file-filter extensions
                                      on-change]]))
 
+(defn log-exceptions [func & args]
+  "Log (i.e. print) exceptions received from the given function."
+  (try
+    (apply func args)
+    (catch Exception e
+      (println (.getMessage e)))))
+
+
 (defn watch
   [change-fn]
   (watcher ["resources/posts/" "resources/pages/"]
            (rate 300)
-           (on-change change-fn)))
+           (on-change (partial log-exceptions change-fn))))
 
 (def watching nil)
 
