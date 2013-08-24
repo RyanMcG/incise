711171024c56fdf7aa31082c3c0667c471750501
Ryan McGowan <ryan@ryanmcg.com>
2013-08-24 01:41:20 -0700

    Fix parser helper spec.

--- spec/incise/parsers/helpers_spec.clj
+++ spec/incise/parsers/helpers_spec.clj
@@ -9,20 +9,20 @@
   (it "handles empty dates well"
     (should= "" (date-str->path "")))
   (it "basically works"
-    (should= "/2013/8/12" (date-str->path "2013-08-12"))))
+    (should= "2013/8/12/" (date-str->path "2013-08-12"))))
 
 (describe "Parse->path"
   (it "works with dated parses"
-    (should= "/2013/8/12/something-about-binding-pry/index.html"
+    (should= "2013/8/12/something-about-binding-pry/index.html"
              (Parse->path (map->Parse {:title "Something about binding.pry"
                                        :date "2013-8-12"
                                        :extension "/index.html"}))))
   (it "works with undated parses"
-    (should= "/run-a-muck/index.html"
+    (should= "run-a-muck/index.html"
              (Parse->path (map->Parse {:title "Run a Muck"
                                        :date ""
                                        :extension "/index.html"})))
-    (should= "/nilnilnil/index.html"
+    (should= "nilnilnil/index.html"
              (Parse->path (map->Parse {:title "NilNilNil"
                                        :date nil
                                        :extension "/index.html"})))))
