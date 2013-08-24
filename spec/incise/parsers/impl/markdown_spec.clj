d334fad9d5f21d56dd00de7c3dd4b733e8f5fab9
Ryan McGowan <ryan@ryanmcg.com>
2013-08-23 17:03:07 -0700

    Fix require expression in markdown-spec.

--- spec/incise/parsers/impl/markdown_spec.clj
+++ spec/incise/parsers/impl/markdown_spec.clj
@@ -2,8 +2,8 @@
   (:require [speclj.core :refer :all]
             [clojure.java.io :refer [file resource]]
             [incise.core] ; Ensure that layouts have been loaded
-            (incise.parsers [core :as pc]
-                            [markdown :refer :all]))
+            [incise.parsers.core :as pc]
+            [incise.parsers.impl.markdown :refer :all])
   (:import [java.io File]))
 
 (describe "parsing"
