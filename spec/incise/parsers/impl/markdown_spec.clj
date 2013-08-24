4dd5c35fdab65797cb7c25555debe3207c515d2b
Ryan McGowan <ryan@ryanmcg.com>
2013-08-24 01:46:39 -0700

    Fix markdown spec.

--- spec/incise/parsers/impl/markdown_spec.clj
+++ spec/incise/parsers/impl/markdown_spec.clj
@@ -2,13 +2,16 @@
   (:require [speclj.core :refer :all]
             [clojure.java.io :refer [file resource]]
             [incise.core] ; Ensure that layouts have been loaded
-            [incise.parsers.core :as pc]
-            [incise.parsers.impl.markdown :refer :all])
+            (incise.parsers [core :as pc]
+                            [html :refer [html-parser]])
+            [incise.parsers.impl.markdown :refer :all]
+            [markdown.core :as md])
   (:import [java.io File]))
 
 (describe "parsing"
   (with markdown-file (file (resource "spec/another-forgotten-binding-pry.md")))
+  (with parser (html-parser md/md-to-html-string))
   (it "does something"
-    (should= "" (pc/Parse->string (parse @markdown-file)))))
+    (should-not-throw (@parser @markdown-file))))
 
 (run-specs)
