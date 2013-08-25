fdd757ff0236dbeec804d3900dcd277c409bcbe6
Ryan McGowan <ryan@ryanmcg.com>
2013-08-25 01:49:46 -0700

    Use cegdown instead of markdown-clj.
    Fenced code blocks ftw.

--- src/incise/parsers/impl/markdown.clj
+++ src/incise/parsers/impl/markdown.clj
@@ -1,6 +1,15 @@
 (ns incise.parsers.impl.markdown
   (:require (incise.parsers [core :as pc]
                             [html :refer [html-parser]])
-            [markdown.core :as md]))
+            [me.raynes.cegdown :as md]))
 
-(pc/register [:md :markdown] (html-parser md/md-to-html-string))
+(def ^:dynamic *options* [:fenced-code-blocks
+                          :autolinks
+                          :quotes
+                          :abbreviations
+                          :tables
+                          :definition-lists
+                          :smarts
+                          :smartypants])
+
+(pc/register [:md :markdown] (html-parser #(md/to-html % *options*)))
