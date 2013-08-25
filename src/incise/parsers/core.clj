3942d004752957f96d19a0112a3a49367d72449f
Ryan McGowan <ryan@ryanmcg.com>
2013-08-24 23:40:50 -0700

    Modify parse to only attempt parsing when a parser exists.
    Also, log the file being parsed.

--- src/incise/parsers/core.clj
+++ src/incise/parsers/core.clj
@@ -3,8 +3,10 @@
             [incise.parsers.helpers :refer [extension]]
             [clj-time.core :as tm]
             [clojure.java.io :refer [file]]
+            [taoensso.timbre :refer [with-default-outs spy trace debug info warn error fatal]]
             [clojure.string :as s])
-  (:import [java.io File]))
+  (:import [java.io File])
+  (:refer-clojure :exclude [contains?]))
 
 (defrecord Parse [^String title
                   ^String extension
@@ -20,14 +22,21 @@
    parse function takes a java.io.File and returns a Parse."
   (atom {}))
 
+(defn contains? [& args]
+  (apply clojure.core/contains? @parsers args))
+
 (defn register
   "Register a parser for the given file extensions."
   [extensions parser]
   (swap! parsers
          merge (zipmap (map name extensions)
                        (repeat parser))))
+
 (defn parse
   "Do all the work, parse the file and output it to the proper location."
   [^File handle]
-  {:pre [(contains? @parsers (extension handle))]}
-  ((@parsers (extension handle)) handle))
+  {:pre [(instance? File handle)]}
+  (let [ext (extension handle)]
+    (when (contains? ext)
+      (info "Parsing" (.getPath handle))
+      ((@parsers ext) handle))))
