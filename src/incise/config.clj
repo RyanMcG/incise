0982a5d4272c29b88cecb00bbf01c82eb219b99b
Ryan McGowan <ryan@ryanmcg.com>
2013-08-24 22:59:12 -0700

    Add load-config to config.

--- src/incise/config.clj
+++ src/incise/config.clj
@@ -1,7 +1,16 @@
 (ns incise.config
+  (:require [clojure.java.io :refer [reader file resource]]
+            [clojure.edn :as edn])
+  (:import [java.io PushbackReader])
   (:refer-clojure :exclude [assoc get]))
 
 (def ^:private config (atom {}))
+
+(defn load-config []
+  (when-let [config-file (file (resource "incise.edn"))]
+    (reset! config (edn/read (PushbackReader. (reader config-file))))))
+
+(load-config)
 
 (defn assoc [& args]
   (apply swap! config assoc args))
