631e947667d139bc56fa1bd7462bd65fcddf9388
Ryan McGowan <ryan@ryanmcg.com>
2013-08-24 23:00:49 -0700

    Modify page layout to take entire parse as second param.

--- src/incise/layouts/impl/page.clj
+++ src/incise/layouts/impl/page.clj
@@ -1,13 +1,20 @@
 (ns incise.layouts.impl.page
   (:require [incise.layouts.core :refer [register]]
+            [incise.parsers.core]
+            [dieter.core :refer [link-to-asset]]
             (hiccup [page :refer :all]
                     [core :refer :all])))
 
-(defn page [{:keys [site-title]} content]
-  (let [{:keys [title date tags category]} (meta content)]
-    (html5
-      [:head [:title (str site-title (when title (str " - " title)))]]
-      [:body content])))
+(defn page [{:keys [site-title]}
+            ^incise.parsers.core.Parse {:keys [title content]}]
+  (html5
+    [:head
+     [:title (str site-title (when title (str " - " title)))]
+     [:meta {:name "viewport"
+             :content "width=device-width, initial-scale=1.0"}]
+     (link-to-asset "stylesheets/app.css.dieter")]
+    [:body#page content
+     (link-to-asset "javascripts/app.js.dieter")]))
 
 (register :page page)
 (register :post page)
