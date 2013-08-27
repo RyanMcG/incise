d1db918eaee56204417e3f3f79421d5977fb10b1
Ryan McGowan <ryan@ryanmcg.com>
2013-08-27 00:41:19 -0700

    Add dieter assets to page layout.

--- src/incise/layouts/impl/page.clj
+++ src/incise/layouts/impl/page.clj
@@ -5,16 +5,19 @@
             (hiccup [page :refer :all]
                     [core :refer :all])))
 
-(defn page [{:keys [site-title]}
-            ^incise.parsers.core.Parse {:keys [title content]}]
+(defn page
+  "The default page/post layout."
+  [{:keys [site-title]}
+   ^incise.parsers.core.Parse {:keys [title content]}]
   (html5
     [:head
      [:title (str site-title (when title (str " - " title)))]
      [:meta {:name "viewport"
              :content "width=device-width, initial-scale=1.0"}]
-     (link-to-asset "stylesheets/app.css.dieter")]
+     (include-css (link-to-asset "stylesheets/app.css.dieter"))]
     [:body#page content
-     (link-to-asset "javascripts/app.js.dieter")]))
+     (include-js "http://code.jquery.com/jquery-2.0.3.min.js"
+                 (link-to-asset "javascripts/app.js.dieter"))]))
 
 (register :page page)
 (register :post page)
