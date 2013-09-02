b23cdf2bb3ede4a0b69ec263afed2824f7e0f36d
Ryan McGowan <ryan@ryanmcg.com>
2013-09-01 22:28:45 -0700

    Add robert/hooke dependency.

--- project.clj
+++ project.clj
@@ -9,6 +9,7 @@
                  [hiccup "1.0.2"]
                  [compojure "1.1.5"]
                  [http-kit "2.1.1"]
+                 [robert/hooke "1.3.0"]
                  [me.raynes/cegdown "0.1.0"]
                  [org.clojure/java.classpath "0.2.0"]
                  [org.clojure/tools.namespace "0.2.4"]
