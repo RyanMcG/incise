7f05b96140c5b4dfc147f3d4727116a63739040c
Ryan McGowan <ryan@ryanmcg.com>
2013-09-03 21:24:58 -0700

    Add merge fn to config ns.

--- src/incise/config.clj
+++ src/incise/config.clj
@@ -2,7 +2,7 @@
   (:require [clojure.java.io :refer [reader file resource]]
             [clojure.edn :as edn])
   (:import [java.io PushbackReader])
-  (:refer-clojure :exclude [load assoc get]))
+  (:refer-clojure :exclude [merge load assoc get]))
 
 (def ^:private config (atom {}))
 
@@ -10,10 +10,14 @@
   (when-let [config-file (file (resource "incise.edn"))]
     (reset! config (edn/read (PushbackReader. (reader config-file))))))
 
-(defn assoc [& args]
-  (apply swap! config assoc args))
+(defn assoc [& more]
+  (apply swap! config clojure.core/assoc more))
 
-(defn get [& args]
-  (if (empty? args)
+(defn get [& more]
+  (if (empty? more)
     @config
-    (apply @config args)))
+    (apply @config more)))
+
+(defn merge [& more]
+  (apply swap! config
+         clojure.core/merge more))
