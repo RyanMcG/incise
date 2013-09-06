4dfa9ed5ea9f4bf6fd50be665beeca79904c02b2
Ryan McGowan <ryan@ryanmcg.com>
2013-09-06 00:42:51 -0700

    Implement and use with-args macro.

--- src/incise/core.clj
+++ src/incise/core.clj
@@ -8,36 +8,66 @@
             (stefon [settings :refer [with-options]]
                     [core :as dc])
             [clojure.java.io :refer [file]]
+            [clojure.tools.cli :refer [cli]]
             [robert.hooke :refer [add-hook]]
             [clojure.string :as s]
             [incise.server :refer [wrap-log-exceptions serve stop-server
                                    defserver]])
   (:import [java.io File]))
 
+(defn- with-args*
+  "A helper function to with-args macro which does all the work.
+
+   1.  Load in the config
+   2.  Parse arguments
+   3.  Merge into config
+   4.  Handle help or continue"
+  [args body-fn]
+  (conf/load)
+  (let [[options cli-args banner]
+        (cli args
+             (:doc (meta (the-ns 'incise.core)))
+             ["-h" "--help" "Print this help." :default false :flag true]
+             ["-i" "--in-dir" "The directory to get source from"]
+             ["-o" "--out-dir" "The directory to put content into"])]
+    (conf/merge options)
+    (if (:help options)
+      (println banner)
+      (body-fn cli-args))
+    (when *file* (System/exit 0))))
+
+(defmacro with-args
+  "Take arguments parsing them using cli and handle help accordingly."
+  [args & body]
+  `(with-args* ~args (fn [~'args] ~@body)))
+
 (defn -main
   "Start the development server and watcher."
   [& args]
-  (apply serve args))
+  (with-args args
+    (serve)))
 
-(defn once [& {:as config}]
-  (load-all)
-  (conf/merge config)
-  (let [out-dir (conf/get :out-dir)
-        stefon-pre-opts {:cache-mode :production
-                         :compress true
-                         :serving-root out-dir
-                         :precompiles (conf/get :precompiles)}]
-    (info "Clearing out" (str \" out-dir \"))
-    (delete-recursively (file out-dir))
-    (with-options stefon-pre-opts
-      (info "Precompiling assets...")
-      (info (with-out-str (dc/precompile stefon-pre-opts)))
-      (info "Done.")
-      (->> (conf/get :in-dir)
-           (file)
-           (file-seq)
-           (map (wrap-log-exceptions parse))
-           (dorun)))))
+(defn once
+  "Incise "
+  [& args]
+  (with-args args
+    (let [out-dir (conf/get :out-dir)
+          stefon-pre-opts {:cache-mode :production
+                           :compress true
+                           :serving-root out-dir
+                           :precompiles (conf/get :precompiles)}]
+      (info "Clearing out" (str \" out-dir \"))
+      (delete-recursively (file out-dir))
+      (with-options stefon-pre-opts
+        (info "Precompiling assets...")
+        (info (with-out-str (dc/precompile)))
+        (info "Done.")
+        (load-parsers-and-layouts)
+        (->> (conf/get :in-dir)
+             (file)
+             (file-seq)
+             (map (wrap-log-exceptions parse))
+             (dorun))))))
 
 (defn restart
   "Define a server and set up a watcher AT THE SAME TIME!"
