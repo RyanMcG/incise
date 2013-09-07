ba8717631819e4511ef5664661899adb74e05ad1
Ryan McGowan <ryan@ryanmcg.com>
2013-09-07 12:49:10 -0700

    Fix up main fn to serve or run once based on args.

--- src/incise/core.clj
+++ src/incise/core.clj
@@ -28,46 +28,51 @@
         (cli args
              (:doc (meta (the-ns 'incise.core)))
              ["-h" "--help" "Print this help." :default false :flag true]
+             ["-1" "--once" "Instead of running in server mode run once."
+              :default false :flag true]
              ["-i" "--in-dir" "The directory to get source from"]
              ["-o" "--out-dir" "The directory to put content into"])]
     (conf/merge options)
     (if (:help options)
-      (println banner)
-      (body-fn cli-args))
-    (when *file* (System/exit 0))))
+      (do (println banner)
+          (System/exit 0))
+      (body-fn cli-args))))
 
 (defmacro with-args
   "Take arguments parsing them using cli and handle help accordingly."
   [args & body]
   `(with-args* ~args (fn [~'args] ~@body)))
 
+(defn once
+  "Incise just once."
+  [& [load-config]]
+  (when-not (false? load-config) (conf/load))
+  (let [out-dir (conf/get :out-dir)
+        stefon-pre-opts {:cache-mode :production
+                         :compress true
+                         :serving-root out-dir
+                         :precompiles (conf/get :precompiles)}]
+    (info "Clearing out" (str \" out-dir \"))
+    (delete-recursively (file out-dir))
+    (with-options stefon-pre-opts
+      (info "Precompiling assets...")
+      (info (with-out-str (dc/precompile)))
+      (info "Done.")
+      (load-parsers-and-layouts)
+      (->> (conf/get :in-dir)
+           (file)
+           (file-seq)
+           (map (wrap-log-exceptions parse))
+           (dorun)))))
+
 (defn -main
   "Start the development server and watcher."
   [& args]
   (with-args args
-    (serve)))
-
-(defn once
-  "Incise "
-  [& args]
-  (with-args args
-    (let [out-dir (conf/get :out-dir)
-          stefon-pre-opts {:cache-mode :production
-                           :compress true
-                           :serving-root out-dir
-                           :precompiles (conf/get :precompiles)}]
-      (info "Clearing out" (str \" out-dir \"))
-      (delete-recursively (file out-dir))
-      (with-options stefon-pre-opts
-        (info "Precompiling assets...")
-        (info (with-out-str (dc/precompile)))
-        (info "Done.")
-        (load-parsers-and-layouts)
-        (->> (conf/get :in-dir)
-             (file)
-             (file-seq)
-             (map (wrap-log-exceptions parse))
-             (dorun))))))
+    (if (conf/get :once)
+      (do (once false)
+          (System/exit 0))
+      (serve))))
 
 (defn restart
   "Define a server and set up a watcher AT THE SAME TIME!"
