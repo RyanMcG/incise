e16c98a7551d2e20a1a229ca0a31f882a604b200
Ryan McGowan <ryan@ryanmcg.com>
2013-09-08 01:03:43 -0700

    Implement basic deploy.core.

--- src/incise/deploy/core.clj
+++ src/incise/deploy/core.clj
@@ -1,3 +1,21 @@
-(ns incise.deploy.core)
+(ns incise.deploy.core
+  (:require [taoensso.timbre :refer [warn]])
+  (:clojure-refer :exclude [get]))
 
-(defn deploy [])
+(def workflows (atom {}))
+
+(defn get [& args]
+  (apply clojure.core/get @workflow args))
+
+(defn deploy []
+  (let [{workflow-name :workflow :as settings} (conf/get :deploy)
+        [workflow (get workflow-name)]]
+    (if workflow
+      (workflow settings)
+      (warn "No workflow registered as" workflow-name))))
+
+(defn register
+  "Register a deployment workflow."
+  [workflow-name workflow]
+  (swap! workflows
+         assoc (name workflow-name) workflow))
