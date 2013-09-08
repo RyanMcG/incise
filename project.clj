60147e269c7166f581b6670e0e9e61797bf65004
Ryan McGowan <ryan@ryanmcg.com>
2013-09-07 20:16:19 -0700

    Add description to project.clj.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject incise "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A hopefully simplified static site generator in Clojure."
+  :url "https://github.com/RyanMcG/incise"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
