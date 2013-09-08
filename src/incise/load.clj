56c050f2ea73b4ca7bade173df5c90da911fc71c
Ryan McGowan <ryan@ryanmcg.com>
2013-09-08 01:02:14 -0700

    Rewrite some load fns to be more generic.

--- src/incise/load.clj
+++ src/incise/load.clj
@@ -5,26 +5,40 @@
 
 (defn- namespace-is-layout-or-parser?
   "Predicate to determine if the given symbol is a namespace for a layout or
-   parser. It would be a namespace under incise.layouts or incise.parsers."
+   parser."
   [namespace-sym]
   (re-find #"incise\.(layouts|parsers)\.impl\..+" (str namespace-sym)))
+
+(defn- namespace-is-deployment-workflow?
+  "Predicate to determine if the given symbol is a namespace for a deployment
+   workflow."
+  [namespace-sym]
+  (re-find #"incise\.deploy\.workflows\..+" (str namespace-sym)))
 
 (defn- namespace-is-spec-or-test?
   "Predicate to determine if the given namespace is a spec or test."
   [namespace-sym]
   (re-find #"-(test|spec)$" (str namespace-sym)))
 
-(defn- find-parser-and-layout-symbols
-  "Find symbols for namespaces on the classpath that are valid layouts and
-   parsers."
-  []
+(defn- filter-namespaces
+  "Find symbols for namespaces on the classpath that pass the given filter
+   function."
+  [filter-fn]
   (->> (classpath)
        (ns-tools/find-namespaces)
        (remove namespace-is-spec-or-test?)
-       (filter namespace-is-layout-or-parser?)))
+       (filter filter-fn)))
 
-(defn load-parsers-and-layouts
-  "Require with reload all namespaces under incise.layouts and incise.parsers."
-  []
-  (doseq [ns-sym (find-parser-and-layout-symbols)]
+(defn- load-ns-syms
+  "Require with reload all namespaces returned by the given fn."
+  [ns-syms-fn]
+  (doseq [ns-sym (ns-syms-fn)]
     (require :reload ns-sym)))
+
+(def load-parsers-and-layouts
+  (partial load-ns-syms
+           (partial filter-namespaces namespace-is-layout-or-parser?)))
+
+(def load-deployment-workflows
+  (partial load-ns-syms
+          (partial filter-namespaces namespace-is-deployment-workflow?)))
