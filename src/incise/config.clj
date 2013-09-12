7e95cd19508ecaeddddf36272615e1bae12c5461
Ryan McGowan <ryan@ryanmcg.com>
2013-09-12 00:53:40 -0700

    Ensure conf/get is valid by throwing an error when it is not.

--- src/incise/config.clj
+++ src/incise/config.clj
@@ -1,6 +1,7 @@
 (ns incise.config
   (:require [clojure.java.io :refer [reader file resource]]
-            [clojure.edn :as edn])
+            [clojure.edn :as edn]
+            [faulter.core :refer [faultless!]])
   (:import [java.io PushbackReader])
   (:refer-clojure :exclude [merge load assoc get]))
 
@@ -13,7 +14,18 @@
 (defn assoc [& more]
   (apply swap! config clojure.core/assoc more))
 
+(def ^:private validations
+  [[(comp string? :in-dir) "must have an input dir"]
+   [(comp string? :out-dir) "must have an output dir"]])
+
+(defn faultless-config!
+  "Throws an AssertionError unless the given config map, or @config if not
+  supplied, has no faults."
+  [& [given-config]]
+  (faultless! 'config validations (or given-config @config)))
+
 (defn get [& more]
+  (faultless-config!)
   (if (empty? more)
     @config
     (apply @config more)))
