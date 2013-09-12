c6b6ccba274051ab89358254814b34e08b9d3f54
Ryan McGowan <mcgowanr@amazon.com>
2013-09-12 14:26:23 -0700

    Update with. Include both dialects by default.

--- spec/manners/with_spec.clj
+++ spec/manners/with_spec.clj
@@ -2,17 +2,32 @@
   (:require [speclj.core :refer :all]
             [manners.with :refer :all]))
 
-(describe "with-validations"
-  (with validations [[odd? "it is odd"]
+(describe "with-etiquette"
+  (with etiquette [[odd? "it is odd"]
                      [#(= 0 (mod % 5)) "it is divisible by 5"]])
   (it "works inside"
-    (with-validations @validations
-      (should (faultless? 5))
-      (should (faulty? 9))
-      (should-contain "it is divisible by 5" (faults 21))
-      (should-throw AssertionError (faultless! 19))
-      (should-not-throw (faultless! 25))))
+    (with-etiquette @etiquette
+      (should (proper? 5))
+      (should (rude? 9))
+      (should-contain "it is divisible by 5" (bad-manners 21))
+      (should-throw AssertionError (avow! 19))
+      (should-not-throw (avow! 25))))
+  (context "works inside"
+    (it "works with victorian"
+      (with-etiquette @etiquette
+      (should (proper? 5))
+      (should (rude? 9))
+      (should-contain "it is divisible by 5" (bad-manners 21))
+      (should-throw AssertionError (avow! 19))
+      (should-not-throw (avow! 25))))
+    (it "works with modern"
+      (with-etiquette @etiquette
+      (should (valid? 5))
+      (should (invalid? 9))
+      (should-contain "it is divisible by 5" (errors 21))
+      (should-throw AssertionError (validate! 19))
+      (should-not-throw (validate! 25)))))
   (it "does not work outside"
-    (should-throw AssertionError (faulty? 9))))
+    (should-throw AssertionError (rude? 9))))
 
 (run-specs)
--- src/manners/with.clj
+++ src/manners/with.clj
@@ -1,16 +1,20 @@
 (ns manners.with
-  (:require [manners.victorian :as victorian]))
+  (:require [manners.modern :as modern]
+            [manners.victorian :as victorian]))
 
-(declare ^:private ^:dynamic *validations*)
+(declare ^:private ^:dynamic *etiquette*)
 
-(doseq [fault-sym ['faults 'faulty?
-                   'faultless? 'faultless! 'manners]]
-  (let [victorian-sym (symbol (str "victorian/" fault-sym))]
-    (eval `(defn ~fault-sym [value#]
-             (~victorian-sym *validations* value#)))))
 
-(defmacro with-validations
-  "Start a scope with the given validations as the base."
-  [validations & body]
-  `(binding [*validations* ~validations]
+(doseq [[dialect syms] {'victorian ['bad-manners 'rude? 'proper? 'avow! 'coach]
+                        'modern ['errors 'invalid? 'valid?
+                                 'validate! 'validator]}]
+  (doseq [sym syms]
+    (let [func-sym (symbol (str dialect \/ sym))]
+      (eval `(defn ~sym [value#]
+               (~func-sym *etiquette* value#))))))
+
+(defmacro with-etiquette
+  "Start a scope with the given etiquettes as the base."
+  [etiquette & body]
+  `(binding [*etiquette* ~etiquette]
      ~@body))
