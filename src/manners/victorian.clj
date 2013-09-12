c8eb60c1548d0d6fc604aa472dfb5a9c4d450f33
Ryan McGowan <mcgowanr@amazon.com>
2013-09-12 14:25:18 -0700

    Victorianify former core.

--- src/manners/victorian.clj
+++ src/manners/victorian.clj
@@ -8,60 +8,60 @@
   (fn [& more]
     (try (apply func more) (catch Exception _))))
 
-(defn- unmemoized-manners
-  "Return a memoized function which takes a value to run the given validations
+(defn- unmemoized-coach
+  "Return a memoized function which takes a value to run the given etiquette
    on."
-  [validations]
-  {:pre [(sequential? validations)]}
+  [etiquette]
+  {:pre [(sequential? etiquette)]}
   (memoize (fn [value]
-             (for [[predicate & message] validations
+             (for [[predicate & message] etiquette
                    :when ((-> predicate wrap-try complement) value)]
                (apply str message)))))
 
-(def manners
+(def coach
   "Create a function from a sequence of validators that returns a sequence of
-   faults."
-  (memoize unmemoized-manners))
+   bad manners."
+  (memoize unmemoized-coach))
 
-(defn faults
-  "Return all faults found with the given validations on the given value."
-  [validations value]
-  ((manners validations) value))
+(defn bad-manners
+  "Return all bad manners found with the given etiquette on the given value."
+  [etiquette value]
+  ((coach etiquette) value))
 
 (defn proper?
-  "A predicate to determine if the given value has any faults according to the
+  "A predicate to determine if the given value has any bad manners according to the
    validations."
-  [validations value]
-  (empty? (faults validations value)))
+  [etiquette value]
+  (empty? (bad-manners etiquette value)))
 
-(def rude? (complement faultless?))
+(def rude? (complement proper?))
 
 (defn falter
-  "Throw an AssertionError when there are faults."
-  ([prefix-sym faults]
+  "Throw an AssertionError when there are bad manners."
+  ([prefix-sym bad-manners]
    (let [full-prefix (when prefix-sym (str "Invalid " prefix-sym ": "))]
-     (when-not (empty? faults)
-       (throw (AssertionError. (str full-prefix (s/join ", " faults)))))))
-  ([faults] (falter nil faults)))
+     (when-not (empty? bad-manners)
+       (throw (AssertionError. (str full-prefix (s/join ", " bad-manners)))))))
+  ([bad-manners] (falter nil bad-manners)))
 
-(defn faultless!
-  "Throw an AssertionError if there are any faults found on the given value with
+(defn avow!
+  "Throw an AssertionError if there are any bad manners found on the given value with
    the given validations."
-  ([prefix validations value] (falter prefix (faults validations value)))
-  ([validations value] (faultless! nil validations value)))
+  ([prefix etiquette value] (falter prefix (bad-manners etiquette value)))
+  ([etiquette value] (avow! nil etiquette value)))
 
-(defmacro demannerss
-  [obj-sym & validations]
-  (let [faults-sym (symbol (str obj-sym "-faults"))
-        faultless?-sym (symbol (str "faultless-" obj-sym \?))
-        faulty?-sym (symbol (str "faulty-" obj-sym \?))
-        faultless!-sym (symbol (str "faultless-" obj-sym \!))]
+(defmacro defmanners
+  [obj-sym & etiquette]
+  (let [bad-manners-sym (symbol (str "bad-" obj-sym "-manners"))
+        proper?-sym (symbol (str "proper-" obj-sym \?))
+        rude?-sym (symbol (str "rude-" obj-sym \?))
+        avow!-sym (symbol (str "avow-" obj-sym \!))]
     `(do
-       (def ~faults-sym
-         (partial faults [~@validations]))
-       (def ~faultless?-sym
-         (partial faultless? [~@validations]))
-       (def ~faulty?-sym
-         (partial faulty? [~@validations]))
-       (defn ~faultless!-sym [value#]
-         (faultless! (quote ~obj-sym) [~@validations] value#)))))
+       (def ~bad-manners-sym
+         (partial bad-manners [~@etiquette]))
+       (def ~proper?-sym
+         (partial proper? [~@etiquette]))
+       (def ~rude?-sym
+         (partial rude? [~@etiquette]))
+       (defn ~avow!-sym [value#]
+         (avow! (quote ~obj-sym) [~@etiquette] value#)))))
