b81faa014d569408bbcb1f14263b174b23f2a3bb
Ryan McGowan <ryan@ryanmcg.com>
2013-09-14 20:44:51 -0700

    Remove manners from incise source.

--- src/manners/utils.clj
+++
@@ -1,7 +0,0 @@
-(ns manners.utils)
-
-(defmacro defalias
-  [name value]
-  `(let [doc-str# (str "This is an alias of " (var ~value)\.)]
-     (def ~name ~value)
-     (alter-meta! (var ~name) assoc :doc doc-str#)))
