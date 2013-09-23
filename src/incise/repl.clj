48db5e9935991c683719df61fe4728d81fa1f391
Ryan McGowan <ryan@ryanmcg.com>
2013-09-22 23:59:47 -0700

    Define start-server and restart-server.

--- src/incise/repl.clj
+++ src/incise/repl.clj
@@ -1,3 +1,13 @@
 (ns incise.repl
   (:require [clojure.tools.namespace.repl :refer :all]
+            [incise.server :refer [stop start]]
+            [incise.config :as conf]
             [incise.core :refer :all]))
+
+(def start-server (wrap-pre start conf/load))
+(defn restart-server
+  "Stop a server if it is already started and start a new one."
+  [& args]
+  (stop)
+  (require :reload 'incise.server)
+  (apply start-server args))
