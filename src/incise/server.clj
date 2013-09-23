a3432fb73d1ad63e1fba305ef659b43e21b6e44a
Ryan McGowan <ryan@ryanmcg.com>
2013-09-22 23:59:30 -0700

    Define and use create-app since routes cannot be defined before config is loaded.

--- src/incise/server.clj
+++ src/incise/server.clj
@@ -1,6 +1,6 @@
 (ns incise.server
   (:require (compojure [route :refer [files not-found]]
-                       [core :refer [defroutes]])
+                       [core :refer [routes]])
             (hiccup [page :refer [html5]])
             (incise [config :as conf]
                     [load :refer [load-parsers-and-layouts]])
@@ -11,10 +11,6 @@
             [taoensso.timbre :refer [info error fatal]]
             [clojure.stacktrace :refer [print-cause-trace]]
             [org.httpkit.server :refer [run-server]]))
-
-(defroutes routes
-  (files "/" {:root (conf/get :out-dir)})
-  (not-found (html5 [:h1 "404"])))
 
 (defn wrap-static-index [handler]
   (fn [{:keys [uri] :as request}]
@@ -34,24 +30,29 @@
            (fatal cause-trace)))
         (when bubble (throw e))))))
 
-(def app (-> routes
-             (wrap-static-index)
-             (wrap-reload :dirs ["src" "spec"])
-             (wrap-incise)
-             (wrap-log-exceptions)
-             (wrap-stacktrace-web)
-             (asset-pipeline)))
+(defn create-app
+  "Create a ring application that is a deverlopment friendly server."
+  []
+  (-> (routes (files "/" {:root (conf/get :out-dir)})
+              (not-found (html5 [:h1 "404"])))
+      (wrap-static-index)
+      (wrap-reload :dirs ["src" "spec"])
+      (wrap-incise)
+      (wrap-log-exceptions)
+      (wrap-stacktrace-web)
+      (asset-pipeline)))
 
-(defn getenv
+(defn- getenv
   "A nice wrapper around System/getenv that allows a second argument to be
   passed in as the default."
   [variable & [default]]
   (or (System/getenv variable) default))
 
 (defn serve
-  "Start the development server"
+  "Start a development server."
   []
-  (let [port (Integer. (conf/get :port (getenv "PORT" 5000)))]
+  (let [port (Integer. (conf/get :port (getenv "PORT" 5000)))
+        app (create-app)]
     (info "Serving at"
           (str "http://" (.getCanonicalHostName
                            (java.net.InetAddress/getLocalHost)) \: port \/))
@@ -60,14 +61,15 @@
                                (conf/get :thread-count
                                          (getenv "THREAD_COUNT" 4)))})))
 
+;; ## Functions for manipulating the server atom.
 (defonce server (atom nil))
 
-(defn defserver
+(defn start
   "Start a server and bind the result to a var, 'server'."
   [& args]
   (reset! server (apply serve args)))
 
-(defn stop-server []
+(defn stop []
   (when @server
     (@server)
     (reset! server nil)))
