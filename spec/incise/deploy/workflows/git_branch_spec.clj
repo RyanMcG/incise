86c2c628a3da010caea9c8ccd017bca7ef78164e
Ryan McGowan <ryan@ryanmcg.com>
2013-10-21 22:42:44 -0700

    Spec out git-branch ns.

--- spec/incise/deploy/workflows/git_branch_spec.clj
+++ spec/incise/deploy/workflows/git_branch_spec.clj
@@ -2,8 +2,9 @@
   (:require [stefon.util :refer [temp-dir]]
             [speclj.core :refer :all]
             (clojure.java [io :refer [file]]
-                         [shell :refer [with-sh-dir]])
-            [clj-jgit [porcelain :refer :all :exclude [with-repo]]]
+                          [shell :refer [with-sh-dir sh]])
+            [clj-jgit [porcelain :refer :all :exclude [with-repo git-push]]]
+            [incise.config :as conf]
             [incise.deploy.workflows.git-branch :refer :all])
   (:import [org.eclipse.jgit.api Git]))
 
@@ -62,5 +63,78 @@
                    ["a" "b" "c"]))
   (it "adds files"
     (should-not-throw (add-files @files))))
+
+(defn- file-exists? [file-like]
+  (.exists (file *work-dir* file-like)))
+
+(describe "branch-exists?"
+  (with-all branch-name "a-branch-name")
+  (with repo-dir (create-dummy-repo))
+  (around [it] (with-repo @repo-dir (it)))
+  (it "should not exist with an inital repo"
+    (should-not (branch-exists? @branch-name)))
+  (it "should exist after creating the branch"
+    (checkout-orphaned-branch @branch-name)
+    (git-commit *repo* "Blarg blarg")
+    (should (branch-exists? @branch-name))))
+
+(describe "checkout-orphaned-branch"
+  (with-all branch-name "testest")
+  (with repo-dir (create-dummy-repo))
+  (around [it] (with-repo @repo-dir (it)))
+  (it "changes the checked out branch"
+    (should= "master" (git-branch-current *repo*))
+    (checkout-orphaned-branch @branch-name)
+    (should= @branch-name (git-branch-current *repo*)))
+  (it "removes all version controlled files leaving ignored ones"
+    (should (file-exists? ".gitignore"))
+    (should (file-exists? "x"))
+    (should (file-exists? "y"))
+    (checkout-orphaned-branch @branch-name)
+    (should-not (file-exists? ".gitignore"))
+    (should (file-exists? "x"))
+    (should-not (file-exists? "y"))))
+
+(describe "setup-branch"
+  (with-all branch-name "stuffs")
+  (with repo-dir (create-dummy-repo))
+  (around [it] (with-repo @repo-dir (it)))
+  (it "creates a new orphaned branch if it does not exist"
+    (setup-branch @branch-name)
+    (should= @branch-name (git-branch-current *repo*)))
+  (it "checks the branch out if it does exist"
+    (should-not (branch-exists? @branch-name))
+    (checkout-orphaned-branch @branch-name)
+    (git-checkout *repo* "master")
+    (setup-branch @branch-name)
+    (should= @branch-name (git-branch-current *repo*))))
+
+(describe "deploy"
+  (with repo-dir (create-dummy-repo))
+  (with repo-dir-path (.getCanonicalPath @repo-dir))
+  (before (conf/merge {:precompiles []
+                       :in-dir @repo-dir-path}))
+  (after (reset! conf/config {}))
+  (around [it] (with-repo @repo-dir (it)))
+  (it "deploys without commit or push"
+    (should-not-throw (deploy {:path @repo-dir
+                               :commit false
+                               :push false})))
+  (it "deploys with commit only"
+    (should-not-throw (deploy {:path @repo-dir
+                               :commit true
+                               :push false})))
+  (context "with a bare clone"
+    (with remote-dir-path (.getCanonicalPath (spec-temp-dir)))
+    (before (git-clone @repo-dir-path @remote-dir-path "origin" "master" true))
+    (before (doto (-> *repo*
+                      (.getRepository)
+                      (.getConfig))
+              (.setString "remote" "origin" "url" @remote-dir-path)
+              (.save)))
+    (it "deploys with commit and push"
+      (should-not-throw (deploy {:path @repo-dir
+                                 :commit true
+                                 :push true})))))
 
 (run-specs)
