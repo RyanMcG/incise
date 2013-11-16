93983055655e455d3d9652a0aa02eb36b4720a4e
Ryan McGowan <ryan@ryanmcg.com>
2013-11-15 21:53:58 -0800

    Modify copy impl to work with new parsers format.
    Returns a delay which returns a sequential collection of created files.

--- src/incise/parsers/impl/copy.clj
+++ src/incise/parsers/impl/copy.clj
@@ -4,12 +4,16 @@
             [clojure.java.io :refer [file copy]]
             [incise.config :as conf]))
 
-(defn parse [input-file]
+(defn parse
+  "An incise parser which simply copies a file based on its relative path to the
+  input directory."
+  [input-file]
   (let [{:keys [in-dir out-dir]} (conf/get)
         input-filename (remove-prefix-from-path in-dir input-file)
         output-file (file out-dir input-filename)]
-    (.mkdirs (.getParentFile output-file))
-    (copy input-file output-file)
-    output-file))
+    (delay
+      (.mkdirs (.getParentFile output-file))
+      (copy input-file output-file)
+      [output-file])))
 
 (pc/register [:copy :cname] parse)
