08b249c6e72af53c8d543dcc36046f663b9e598e
Ryan McGowan <ryan@ryanmcg.com>
2013-11-23 21:56:42 -0800

    Link to incise svg logo instead of slurping it in.

--- resources/content/index.clj
+++ resources/content/index.clj
@@ -3,14 +3,14 @@
 
 (require '[incise.parsers.impl.markdown :refer [markdown-to-html]])
 (require '[hiccup.core :refer [html]])
-(require '[hiccup.element :refer [link-to]])
+(require '[hiccup.element :refer [link-to image]])
 (require '[clojure.string :refer [replace-first]])
 
 (def md-source (slurp "README.md"))
-(def svg-logo (slurp "assets/logo.svg"))
+(def svg-logo "/assets/images/logo.svg")
 (def svg-logo-link
-  (html (link-to "https://github.com/RyanMcG/incise" svg-logo)))
-
+  (html (link-to "https://github.com/RyanMcG/incise"
+                 (image {:id "logo"} svg-logo "incise logo"))))
 (-> md-source
     (markdown-to-html)
     (replace-first #"\<img.*/\> " svg-logo-link))
