f6a1e166e955ddb754e0e9393185321b098bf668
Ryan McGowan <ryan@ryanmcg.com>
2013-11-23 21:58:20 -0800

    Keep track of config file in config ns.
    Also add serving? function which checks to see the current method is serving.

--- src/incise/config.clj
+++ src/incise/config.clj
@@ -7,21 +7,6 @@
 
 (defonce config (atom {}))
 
-(defn load []
-  (when-let [config-file (file (resource "incise.edn"))]
-    (reset! config (edn/read (PushbackReader. (reader config-file))))))
-
-(defn assoc [& more]
-  (apply swap! config clojure.core/assoc more))
-
-(defmannerisms config
-  [:in-dir "must have an in-dir"]
-  [(comp string? :in-dir) "in-dir must be a string (like a path)"]
-  [:out-dir "must have an output dir"]
-  [(comp string? :out-dir) "out-dir must be a string (like a path)"])
-
-(defn avow! [] (avow-config! @config))
-
 (defn get [& more]
   (if (empty? more)
     @config
@@ -30,3 +15,25 @@
 (defn merge [& more]
   (apply swap! config
          clojure.core/merge more))
+
+(defn assoc [& more]
+  (apply swap! config clojure.core/assoc more))
+
+(defonce config-path (atom nil))
+
+(defn serving? [] (= (get :method) :serve))
+
+(defn load
+  "Load the config from "
+  [& [path-to-config]]
+  (when path-to-config (reset! config-path path-to-config))
+  (when-let [config-file (file (or @config-path (resource "incise.edn")))]
+    (reset! config (edn/read (PushbackReader. (reader config-file))))))
+
+(defmannerisms config
+  [:in-dir "must have an in-dir"]
+  [(comp string? :in-dir) "in-dir must be a string (like a path)"]
+  [:out-dir "must have an output dir"]
+  [(comp string? :out-dir) "out-dir must be a string (like a path)"])
+
+(defn avow! [] (avow-config! @config))
