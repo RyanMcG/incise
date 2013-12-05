4d548cfa669c2e778396a28ce5242ae5bdc03cdc
Ryan McGowan <ryan@ryanmcg.com>
2013-12-04 20:33:17 -0800

    Make head a partial in base layout.

--- src/incise/layouts/impl/base.clj
+++ src/incise/layouts/impl/base.clj
@@ -13,25 +13,6 @@
                     [element :refer :all]))
   (:import [java.io FileNotFoundException]))
 
-(defpartial header "A blank header" [] nil)
-
-(defpartial content
-  "A very basic content partial."
-  [_ {:keys [content]}]
-  (html
-    (condp #(%1 %2) content
-      list? (eval-with-context content)
-      content)))
-
-(defpartial footer
-  "A very basic footer crediting this project."
-  [{:keys [contacts author]} _]
-  (html
-    [:footer
-     [:p
-      "This website was "
-      (link-to "https://github.com/RyanMcG/incise" "incised") \.]]))
-
 (defn- attempt-link-to-asset
   "Return nil if the asset is not found othewise return a url for the asset."
   [asset-name]
@@ -45,17 +26,40 @@
 (defn javascripts []
   [(attempt-link-to-asset "javascripts/app.js.stefon")])
 
+(defpartial head
+  "The default head."
+  [{:keys [site-title]} {:keys [title]}]
+  [:head
+   (when (or site-title title)
+     [:title (s/join " - " (keep identity [site-title title]))])
+   [:meta {:charset "UTF-8"}]
+   [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
+   (apply include-css (keep identity (stylesheets)))])
+
+(defpartial header "A blank header" [] nil)
+
+(defpartial content
+  "A very basic content partial."
+  [_ {:keys [content]}]
+  (html
+    (condp #(%1 %2) content
+      list? (eval-with-context content)
+      content)))
+
+(defpartial footer
+  "A very basic footer crediting this project."
+  [{:keys [contacts author]} _]
+  [:footer
+   [:p
+    "This website was "
+    (link-to "https://github.com/RyanMcG/incise" "incised") \.]])
+
 (deflayout base
   "The default page/post layout."
-  [{:keys [site-title]} {:keys [title]}]
+  []
   (with-base-url (when-not (conf/serving?) (str \/ (conf/get :uri-root)))
     (html5
-      [:head
-       (when (or site-title title)
-         [:title (s/join " - " (keep identity [site-title title]))])
-       [:meta {:charset "UTF-8"}]
-       [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
-       (apply include-css (remove nil? (stylesheets)))]
+      (head)
       [:body#page
        [:div.container
         (header)
