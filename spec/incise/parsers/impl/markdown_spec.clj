47f5dc819ac14e255624e77420c63e70477a6683
Ryan McGowan <ryan@ryanmcg.com>
2014-01-16 22:00:05 -0800

    Load layouts in markdown parsing spec.
    Otherwise it cannot run on its own. Refactor a bit too (DRY it up).

--- spec/incise/parsers/impl/markdown_spec.clj
+++ spec/incise/parsers/impl/markdown_spec.clj
@@ -9,25 +9,20 @@
             [me.raynes.cegdown :as md])
   (:import [java.io File]))
 
-(defn parse-markdown []
-  (let [markdown-file (file (resource "spec/markdown-options.md"))
-        parse (comp slurp first force (html-parser markdown-to-html))]
-    (parse markdown-file)))
-
 (describe "parsing markdown"
+  (before (load-parsers-and-layouts))
+  (with markdown-file (file (resource "spec/markdown-options.md")))
+  (around-with-custom-config :in-dir "resources/spec"
+                             :out-dir "/tmp/")
+  (with parse (comp slurp first force (html-parser markdown-to-html)))
+  (with result (@parse @markdown-file))
   (context "parsing with default options"
-    (around-with-custom-config :in-dir "resources/spec"
-                               :out-dir "/tmp/")
-    (with result (parse-markdown))
     (it "parses a markdown file into html"
         (should-contain #"<html>" @result))
     (it "parses without hardwraps"
         (should-contain "First line Second line" @result)))
   (context "parsing with custom options"
-    (around-with-custom-config :in-dir "resources/spec"
-                               :out-dir "/tmp/"
-                               :parsers {:markdown {:extensions [:hardwraps]}})
-    (with result (parse-markdown))
+    (around-with-custom-config :parsers {:markdown {:extensions [:hardwraps]}})
     (it "parses with hardwraps"
       (should-contain "First line<br/>Second line" @result))))
 
