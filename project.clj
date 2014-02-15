32911a077b96beecad6e3db5eec99903d528d585
Ryan McGowan <ryan@ryanmcg.com>
2014-02-15 13:55:07 -0800

    Inc to 0.2.0 and change deps to stable versions.

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject incise "0.2.0-SNAPSHOT"
+(defproject incise "0.2.0"
   :description "A hopefully simplified static site generator in Clojure."
   :url "http://www.ryanmcg.com/incise/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[incise-markdown-parser "0.1.0-SNAPSHOT"]
-                 [incise-git-deployer "0.1.0-SNAPSHOT"]
-                 [incise-base-hiccup-layouts "0.1.0-SNAPSHOT"]
-                 [incise-core "0.2.0-SNAPSHOT"]]
+  :dependencies [[incise-markdown-parser "0.1.0"]
+                 [incise-git-deployer "0.1.0"]
+                 [incise-base-hiccup-layouts "0.1.0"]
+                 [incise-core "0.2.0"]]
   :profiles {:dev {:source-paths ["dev"]
-                   :dependencies [[incise-stefon "0.1.0-SNAPSHOT"]]}}
+                   :dependencies [[incise-stefon "0.1.0"]]}}
   :repl-options {:init-ns incise.repl}
   :main incise.core)
