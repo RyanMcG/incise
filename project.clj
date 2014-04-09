60dba822f48ea4ebb81009bd90f800ac2437ff8c
Ryan McGowan <ryan@ryanmcg.com>
2014-04-08 20:06:18 -0700

    Bump incise-base-hiccup-layouts to 0.2.0.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[incise-markdown-parser "0.1.0"]
                  [incise-git-deployer "0.1.0"]
-                 [incise-base-hiccup-layouts "0.1.0"]
+                 [incise-base-hiccup-layouts "0.2.0"]
                  [incise-core "0.3.2"]]
   :profiles {:dev {:source-paths ["dev"]
                    :dependencies [[incise-stefon "0.1.0"]]}}
