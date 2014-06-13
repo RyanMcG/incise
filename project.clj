4107318c1f9d4dd13745af27cb1817019d60646f
Ryan McGowan <ryan@ryanmcg.com>
2014-06-12 22:04:06 -0700

    Update deps and bump version to 0.4.0.

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject incise "0.4.0-SNAPSHOT"
+(defproject incise "0.4.0"
   :description "A hopefully simplified static site generator in Clojure."
   :url "http://www.ryanmcg.com/incise/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[incise-markdown-parser "0.1.0"]
+  :dependencies [[incise-markdown-parser "0.2.0"]
                  [incise-git-deployer "0.1.0"]
-                 [incise-base-hiccup-layouts "0.2.0"]
-                 [incise-core "0.3.2"]]
+                 [incise-base-hiccup-layouts "0.3.0"]
+                 [incise-core "0.4.0"]]
   :profiles {:dev {:dependencies [[incise-stefon "0.1.0"]
                                   [com.ryanmcg/incise-vm-layout "0.4.0"]]}}
   :repl-options {:init-ns incise.repl}
